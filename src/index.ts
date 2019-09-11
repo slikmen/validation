31ab2ba779cefd59f99d4ccb937fa35e0d8eb4ec
Jesse <jesse@vergelijkdirect.com>
2019-09-11 13:11:30 +0200

    Add basic vue plugin code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export default {
+    install(Vue: any, options: any) {
+        // 1. add global method or property
+        Vue.myGlobalMethod = function () {
+        // some logic ...
+        }
+    
+        // 2. add a global asset
+        Vue.directive('my-directive', {
+            bind (el: any, binding: any, vnode: any, oldVnode: any) {
+                // some logic ...
+            }
+        })
+    
+        // 3. inject some component options
+        Vue.mixin({
+            created: function () {
+                console.log('hello')
+            }
+        })
+    
+        // 4. add an instance method
+        Vue.prototype.$myMethod = function (methodOptions: any) {
+        // some logic ...
+        }
+    }
+}
