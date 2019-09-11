57231efb465010d4b7b416ae2aefbad1f348a590
Jesse <jesse@vergelijkdirect.com>
2019-09-11 17:24:25 +0200

    Update create code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import MyComponent from './components/Component.vue'
+// import MyComponent from './components/Component.vue'
 
 export default {
     install(Vue: any, options: any) {
         // 1. add global method or property
-        Vue.component('test-validation', MyComponent)
+        // Vue.component(MyComponent.name, MyComponent)
         Vue.myGlobalMethod = () => {
         // some logic ...
         }
@@ -11,20 +11,40 @@ export default {
         // 2. add a global asset
         Vue.directive('my-directive', {
             bind (el: any, binding: any, vnode: any, oldVnode: any) {
-                // some logic ...
+                // Bind directive
             }
         })
     
         // 3. inject some component options
         Vue.mixin({
-            created: () => {
-                // Hello
+            methods: {
+                getAllInputs(el: any) {
+                    return el.getElementsByTagName('input');
+                }
+            },
+            created: (el: any) => {
+                let inputs = Vue.getAllInputs(el)
+                inputs.map((input: any) => {
+                    let fieldName = input.srcElement.name
+                    const field: any = Vue.$validator.fields.find({ name: fieldName })
+
+                    input.addEventListener('blur', (x: any) => {
+                        input.style.borderColor = 'blue'
+                        field.reset()
+                        Vue.$validator.errors.remove(field.name, field.scope)
+                    })
+
+                    input.addEventListener('focus', (x: any) => {
+                        input.style.borderColor = 'red'
+                        Vue.$validator.validate(field.name)
+                    })
+                })
             }
         })
     
         // 4. add an instance method
-        Vue.prototype.$myMethod = (methodOptions: any) => {
-            // some logic ...
+        Vue.prototype.$myMethod = (methodOptions: any, el: any) => {
+        //    Method
         }
     }
 }
